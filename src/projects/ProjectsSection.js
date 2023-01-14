d48910f4f8feba46112d4d5b7613dc985e263a8d
Ajay Bhattacharyya <AjayBhatta16@github.com>
2023-01-13 22:26:06 -0500

    fixed typo

--- src/projects/ProjectsSection.js
+++ src/projects/ProjectsSection.js
@@ -33,7 +33,7 @@ let cards = [
    		mainLink:"https://filedrop.ajaybhatta49.repl.co",
 		srcLink: "https://github.com/AjayBhatta16/filedrop",
    		title:'FileDrop',
-   		description:'A file-sharing web app built using Angular JS, Bootstrap, Flask, and MongoDB..'
+   		description:'A file-sharing web app built using Angular JS, Bootstrap, Flask, and MongoDB.'
  	}
 ]
 
