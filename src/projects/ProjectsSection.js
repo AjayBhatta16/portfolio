484927326a230ef7371566efca3d61deb1e21a04
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-08-03 19:52:54 -0400

    1. Update card component

--- src/projects/ProjectsSection.js
+++ src/projects/ProjectsSection.js
@@ -1,51 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from './Card.js'
 import BodyNav from '../BodyNav.js'
-
-let cards = [
-	{
-		id: 1,
-		imgLink: "images/socialguard.png",
-		mainLink: "https://socialguard-4efb8.firebaseapp.com/",
-		srcLink: "https://github.com/AjayBhatta16/social-media-scam-detector",
-		title: "Social Guard",
-		description: "An AI-Driven web application for identifying fraudulent accounts on social media platforms, built using ReactJS and Flask."
-	},
-	{
-		id: 2,
-		imgLink: "images/bank.png",
-		mainLink: "https://mikeoxlongbanking.firebaseapp.com",
-		srcLink: "https://github.com/AjayBhatta16/fake-bank",
-		title: "Online Bank",
-		description: "An online banking website built using React, Bootstrap, and ExpressJS."
-	},
-	{
-		id: 3,
-		imgLink: "images/grabify.png",
-		mainLink: "https://linkwire.cc",
-		srcLink: "https://github.com/AjayBhatta16/grabify-clone",
-		title: "LinkWire",
-		description: "An IP logging and traffic analysis platform similar to Grabify, built using Bootstrap, ExpressJS, and GCP Firestore",
-	},
- 	{
-   		id: 4,
-   		imgLink:"images/filedrop.png",
-   		mainLink:"https://filedrop.cc",
-		srcLink: "https://github.com/AjayBhatta16/filedrop",
-   		title:'FileDrop',
-   		description:'A file-sharing web app built using Angular JS, Bootstrap, Flask, and MongoDB.'
- 	}
-]
+import { getProjects } from './projects.api.js'
 
 export default function ProjectsSection() {
-  let cardComponents = cards.map(card => <Card key={card.id} data={card} />)
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+	getProjects().then(setCards);
+  }, [])
+
   return (
     <>
     <BodyNav section='projects'/>
     <section id='projects'>
       <h1>My Projects</h1>
       <div className='flexbox'>
-         {cardComponents}
+         {
+		 	cards.map(card => 
+				<Card key={card.id} data={card} />
+			)
+		 }
       </div>
     </section>
     </>
