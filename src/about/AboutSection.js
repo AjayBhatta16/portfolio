cb8a3fabb6a34b1cc5691efc1c3e0fcd48a8e658
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-08-03 23:05:15 -0400

    4. Refactor resume page to use CDN, setup proxy resume URL + 5. Remove unused files

--- src/about/AboutSection.js
+++ src/about/AboutSection.js
@@ -9,7 +9,7 @@ export default function AboutSection() {
       <h1>About Me</h1>
       <p>My name is Ajay Bhattacharyya. I am a software engineer from Rochester, New York, specializing in full-stack development and applied AI systems.</p>
       <p>I graduated from SUNY Brockport in 2024 with Bachelor's degrees in Computer Science and Math, and minors in Business Administration and Computer Information Systems.</p>
-      <a href="resume.pdf" target="_blank"><button>My Resume</button></a>
+      <a href="https://portfolio-assets-2.storage.googleapis.com/resume.pdf" target="_blank"><button>My Resume</button></a>
     </section>
     </>
   )
