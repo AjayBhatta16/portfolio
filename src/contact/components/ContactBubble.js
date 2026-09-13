21af8d8bf881d333778c6e37d7b55c71cfc75e41
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2026-09-13 03:30:57 -0400

    details section working

--- src/contact/components/ContactBubble.js
+++ src/contact/components/ContactBubble.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 
 export default function ContactBubble(props) {
+    const [displayDetails, setDisplayDetails] = useState(false);
+
     const contactInfo = props.data;
 
+    function handleCopy() {
+        navigator.clipboard.writeText(contactInfo.copyText)
+    }
+
     return (
-        <div className='contact-bubble'>
-            <i className={`contact-icon ${contactInfo.iconClassList}`}></i>
+        <div className='flex-col'>
+            <div className='contact-bubble' onClick={() => setDisplayDetails(!displayDetails)}>
+                <i className={`contact-icon ${contactInfo.iconClassList}`}></i>
+            </div>
+            {
+                displayDetails && (
+                    <div className='contact-details flex-col'>
+                        <h4>{contactInfo.name}</h4>
+                        <p>{contactInfo.linkDisplayText}</p>
+                        <div className='flex-row contact-detail-icons'>
+                            <a href={contactInfo.linkUrl} target="_blank" rel="noopener noreferrer">
+                                <i className="fas fa-external-link-alt"></i>
+                            </a>
+                            <i className="far fa-copy" onClick={handleCopy}></i>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
